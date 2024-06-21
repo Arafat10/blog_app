b3e22a972e2c64530ca74e9f403df2de9881c767
Arafat10 <arafat.nabil.10@gmail.com>
2024-06-21 20:40:06 +0600

    signup_page_1

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/features/auth/presentation/widgets/auth_field.dart';
 import 'package:flutter/material.dart';
 
 class SignupPage extends StatefulWidget {
@@ -11,17 +12,26 @@ class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Sign Up",
-            style: TextStyle(
-              fontSize: 50,
-              fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Sign Up',
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-        ],
+            const SizedBox(height: 30),
+            AuthField(hintText: 'Name'),
+            const SizedBox(height: 15),
+            AuthField(hintText: 'Email'),
+            const SizedBox(height: 15),
+            AuthField(hintText: 'Password'),
+          ],
+        ),
       ),
     );
   }
